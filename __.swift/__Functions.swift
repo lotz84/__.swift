0cb7bbe89a5be8c7d79dc47cd5dff3487a050d83
tatyusa <tatsuya.hirose.0804@gmail.com>
2014-06-07 16:55:38 +0900

    Add partial function and test

---
+++ __.swift/__Functions.swift
@@ -0,0 +1,44 @@
+//
+//  __Functions.swift
+//  __.swift
+//
+//  Created by Hirose Tatsuya on 2014/06/07.
+//  Copyright (c) 2014 tatsuya hirose. All rights reserved.
+//
+
+import Foundation
+
+
+extension __ {
+    
+    // partially act to function which takes 2 variables
+    class func partial<T, U, V>(f: (T, U) -> V, arg: T ) -> ( U -> V ) {
+        return { f(arg, $0) }
+    }
+    
+    // partially act to function which takes 3 variables
+    class func partial<T, U, V, W>(f: (T, U, V) -> W, arg: T ) -> ( (U , V) -> W ) {
+        return { f(arg, $0, $1) }
+    }
+    
+    // partially act to function which takes 4 variables
+    class func partial<T, U, V, W, X>(f: (T, U, V, W) -> X, arg: T ) -> ( (U , V, W) -> X ) {
+        return { f(arg, $0, $1, $2) }
+    }
+    
+    // partially act to function which takes 5 variables
+    class func partial<T, U, V, W, X, Y>(f: (T, U, V, W, X) -> Y, arg: T ) -> ( (U , V, W, X) -> Y ) {
+        return { f(arg, $0, $1, $2, $3) }
+    }
+    
+    // partially act to function which takes 6 variables
+    class func partial<T, U, V, W, X, Y, Z>(f: (T, U, V, W, X, Y) -> Z, arg: T ) -> ( (U , V, W, X, Y) -> Z ) {
+        return { f(arg, $0, $1, $2, $3, $4) }
+    }
+    
+    // partially act to function which takes 7 variables
+    class func partial<T, U, V, W, X, Y, Z, A>(f: (T, U, V, W, X, Y, Z) -> A, arg: T ) -> ( (U , V, W, X, Y, Z) -> A ) {
+        return { f(arg, $0, $1, $2, $3, $4, $5) }
+    }
+    
+}
---
+++ __.swiftTests/__FunctionsTests.swift
@@ -0,0 +1,24 @@
+//
+//  __FunctionsTests.swift
+//  __.swift
+//
+//  Created by Hirose Tatsuya on 2014/06/07.
+//  Copyright (c) 2014 tatsuya hirose. All rights reserved.
+//
+
+import XCTest
+
+class ___FunctionsTests: XCTestCase {
+
+    func testPartial(){
+        let add = { (x: Int, y :Int) in (x+y, x, y) }
+        let add5 = __.partial(add, arg: 5)
+        XCTAssert(add5(10).0==15)
+        
+        let triadd = { (x: Int, y: Int, z: Int) in x+y+z }
+        let add6 = __.partial(triadd, arg: 6)
+        let add6and7 = __.partial(add6, arg: 7)
+        XCTAssert(add6and7(10)==23)
+    }
+    
+}
